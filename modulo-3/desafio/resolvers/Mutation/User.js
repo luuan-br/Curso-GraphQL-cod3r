748aef260c815d0e7e41eccd2e396feee733fef2
Luan Silva <luuan.br@live.com>
2020-01-17 10:12:08 -0300

    Terminando modulo 3

--- modulo-3/desafio/resolvers/Mutation/User.js
+++ modulo-3/desafio/resolvers/Mutation/User.js
@@ -1,29 +1,34 @@
 /** @format */
 const db = require("../../config/db")
+const { profile: getProfile } = require("../Query/Profile")
 const { user: getUser } = require("../Query/User")
 
 module.exports = {
 	async newUser(_, { data }) {
 		try {
-			const idProfiles = []
+			const idsProfiles = []
 
 			if (data.profiles) {
-				for (const filtre of data.profiles) {
-					const profile = await getUser(_, { filtre })
+				for (let profileFiltre of data.profiles) {
+					const profile = await getProfile(_, { filtre: { ...profileFiltre } })
 
-					if (profile) idProfiles.push(profile.id)
+					if (profile) {
+						idsProfiles.push(profile.id)
+					}
 				}
 			}
 
-			const [id] = db("usuario").insert({
+			// delete data.profiles
+
+			const [id] = await db("usuarios").insert({
 				nome: data.name,
 				email: data.email,
 				senha: data.password,
 			})
 
-			for (const perfil_id of idProfiles) {
-				await db("usuarios_perfils").insert({
-					perfil_id,
+			for (let profile_id of idsProfiles) {
+				await db("usuarios_perfis").insert({
+					perfil_id: profile_id,
 					usuario_id: id,
 				})
 			}
@@ -36,7 +41,67 @@ module.exports = {
 		}
 	},
 
-	deleteUser(_, { filtre }) {},
+	async deleteUser(_, args) {
+		try {
+			const user = await getUser(_, args)
 
-	changeUser(_, { filtre, data }) {},
+			if (user) {
+				const { id } = user
+
+				await db("usuarios_perfis")
+					.where({ usuario_id: id })
+					.delete()
+
+				await db("usuarios")
+					.where({ id })
+					.delete()
+			}
+
+			return user
+		} catch (e) {
+			throw new Error(e.sqlMessage)
+		}
+	},
+
+	async changeUser(_, { filtre, data }) {
+		try {
+			const user = await getUser(_, { filtre })
+
+			if (user) {
+				const { id } = user
+
+				if (data.profiles) {
+					await db("usuarios_perfis")
+						.where({ usuario_id: id })
+						.delete()
+
+					for (let filtre of data.profiles) {
+						const profile = await getProfile(_, { filtre })
+
+						profile &&
+							(await db("usuarios_perfis").insert({
+								perfil_id: profile.id,
+								usuario_id: id,
+							}))
+					}
+				}
+
+				await db("usuarios")
+					.where({ id })
+					.update({
+						nome: data.name ? data.name : user.nome,
+						email: data.email ? data.email : user.email,
+						senha: data.password ? data.password : user.senha,
+					})
+			}
+
+			return user
+				? db("usuarios")
+						.where({ id: user.id })
+						.first()
+				: null
+		} catch (e) {
+			throw new Error(e.sqlMessage)
+		}
+	},
 }
